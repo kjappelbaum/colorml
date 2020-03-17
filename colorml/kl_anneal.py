7aabd210a70dc72c31a720e01f03e65b5e2a14f4
Kevin <kevin.jablonka@epfl.ch>
2020-03-17 11:55:44 +0100

    fix: kl schedule

--- colorml/kl_anneal.py
+++ colorml/kl_anneal.py
@@ -6,4 +6,4 @@ def monotonical_kl_anneal(epoch):
     To use the standard monotonical anneal as in Bowman et al. 2015
     """
     M = 20
-    return np.min(np.tanh(1 / M * epoch), 1)
+    return np.min([np.tanh(1 / M * epoch), 1])
